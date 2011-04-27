d0c34c0d69a7eba1d8ad582e5613ceb8d2ce1b24
unknown <jwesonga@.I-TECHKENYA.ORG>
2011-04-27 22:54:46 +0300

    added models package,Story model, updated SeekikaDbAdapter

--- src/com/seekika/android/app/database/SeekikaDbAdapter.java
+++ src/com/seekika/android/app/database/SeekikaDbAdapter.java
@@ -1,21 +1,34 @@
 package com.seekika.android.app.database;
 
+import com.seekika.android.app.models.Story;
+
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 public class SeekikaDbAdapter {
+	
 	 	private static final String TAG = "SeekikaDbAdapter";
+	 	private static final String CREATE_STORY_TABLE="";
 	    private DatabaseHelper mDbHelper;
 	    private SQLiteDatabase mDb;
 	    
 	    private static final String DATABASE_NAME = "Seekika";
-	    private static final String DATABASE_TABLE = "stories";
+	    private static final String STORY_TABLE = "Story";
 	    private static final int DATABASE_VERSION = 2;
 	    
+	    //Story table
+	    private static final String STORY_ID="_id";
+	    
+	    
+	    private Context applicationContext;
+	    
 	private static class DatabaseHelper extends SQLiteOpenHelper {
+		
 
 		public DatabaseHelper(Context context) {
 			super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -24,7 +37,7 @@ public class SeekikaDbAdapter {
 
 		@Override
 		public void onCreate(SQLiteDatabase db) {
-			// TODO Auto-generated method stub
+			db.execSQL(CREATE_STORY_TABLE);
 			
 		}
 
@@ -32,10 +45,26 @@ public class SeekikaDbAdapter {
 		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 			Log.w(TAG,"Upgrading database from version " + oldVersion + " to "
                     + newVersion + ", which will destroy all old data");
-			db.execSQL("DROP TABLE IF EXISTS stories");
+			db.execSQL("DROP TABLE IF EXISTS story");
             onCreate(db);
 			
 		}
 		
+	}
+	
+	public SeekikaDbAdapter(Context context){
+		this.applicationContext=context;
+	}
+	
+	public SeekikaDbAdapter open() throws SQLException{
+		mDbHelper=new DatabaseHelper(applicationContext);
+		mDb=mDbHelper.getWritableDatabase();
+		return this;
+	}
+	//Method to create story/recording
+	public long addStory(Story story){
+		ContentValues initialValues = new ContentValues();
+		
+		return mDb.insert(STORY_TABLE, null, initialValues);
 	}
 }
